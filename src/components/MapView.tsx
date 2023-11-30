c2938674bb5677b086af81204982d7f22545213a
devferx <ferkki@hotmail.com>
2023-11-30 18:43:53 -0400

    Add mapbox-gl and related dependencies

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,13 +1,40 @@
-import { useContext } from "react";
+import { useContext, useLayoutEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
 import { PlacesContext } from "../context";
-import { Loading } from ".";
+import { Loading } from "./";
 
 export const MapView = () => {
   const { isLoading, userLocation } = useContext(PlacesContext);
+  const mapDiv = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    if (!isLoading && mapDiv.current) {
+      new mapboxgl.Map({
+        container: mapDiv.current,
+        style: "mapbox://styles/mapbox/streets-v12", // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return <div>{userLocation?.join(",")}</div>;
+  return (
+    <div
+      ref={mapDiv}
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
+      {userLocation?.join(",")}
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 import { MapsApp } from "./MapsApp.tsx";
 import "./index.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 if (!navigator.geolocation) {
   window.alert("Tu navegador no soporta geolocalización");
   throw new Error("Tu navegador no soporta geolocalización");
 }
+
+mapboxgl.accessToken = import.meta.env.VITE_APP_MAPBOX_API_KEY;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
