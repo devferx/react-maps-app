0e474127304eaa453091295f6f21f1d9d1bee986
devferx <ferkki@hotmail.com>
2023-12-01 14:48:29 -0400

    Add map functionality and active place highlighting

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,22 +1,45 @@
-import { useContext } from "react";
-import { PlacesContext } from "../context";
+import { useContext, useState } from "react";
+import { MapContext, PlacesContext } from "../context";
 import { LoadingPlaces } from ".";
+import { Feature } from "../interfaces/places";
 
 export const SearchResults = () => {
   const { places, isLoadingPlaces } = useContext(PlacesContext);
+  const { map } = useContext(MapContext);
+
+  const [activeId, setActiveId] = useState("");
 
   if (isLoadingPlaces) return <LoadingPlaces />;
   if (places.length === 0) return <></>;
 
+  const onPlaceClicked = (place: Feature) => {
+    setActiveId(place.id);
+    const [lng, lat] = place.center;
+    map?.flyTo({
+      zoom: 14,
+      center: [lng, lat],
+    });
+  };
+
   return (
     <ul className="list-group mt-3">
       {places.map((place) => (
-        <li key={place.id} className="list-group-item list-group-item-action">
+        <li
+          key={place.id}
+          className={`list-group-item list-group-item-action pointer ${
+            activeId === place.id ? "active" : ""
+          }`}
+          onClick={() => onPlaceClicked(place)}
+        >
           <h6>{place.text_es}</h6>
-          <p className="text-muted" style={{ fontSize: "12px" }}>
-            {place.place_name}
-          </p>
-          <button className="btn btn-outline-primary btn-sm">
+          <p style={{ fontSize: "12px" }}>{place.place_name}</p>
+          <button
+            className={`btn btn-sm ${
+              activeId === place.id
+                ? "btn-outline-light"
+                : "btn-outline-primary"
+            }`}
+          >
             Direcciones
           </button>
         </li>
